a939702125ba28abb41b34db4acdd646074970b0
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-12-31 15:58:31 -0300

    add textarea

--- lib/elixir_gist_web/live/create_gist_live.ex
+++ lib/elixir_gist_web/live/create_gist_live.ex
@@ -3,7 +3,7 @@ defmodule ElixirGistWeb.CreateGistLive do
   alias ElixirGist.{Gists, Gists.Gist}
 
   def mount(_params, _session, socket) do
-     socket = assign(socket, form: to_form(Gists.change_gist(%Gist{})))
+    socket = assign(socket, form: to_form(Gists.change_gist(%Gist{})))
     {:ok, socket}
   end
 end
